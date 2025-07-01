7a0410c9fea910172adf5bcf6a96a92c51e4e510
MBSA <marinacompsci@protonmail.com>
2025-07-01 19:52:32 +0200

    fix: use default dict

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # RUN THIS INSIDE .git/hooks OF YOUR GIT REPO
 
-cat extra_words | xargs -I {} echo -e "*{}\\n#" | hunspell -d en_US -a
+cat extra_words | xargs -I {} echo -e "*{}\\n#" | hunspell -a
 
 ln -s $HOME/developer/git-hooks/commit-msg .git/hooks
 ln -s $HOME/developer/git-hooks/pre-commit .git/hooks
