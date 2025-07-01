28fa532fe87db7b3c2590fae7a23f6295d63d7f2
MBSA <marinacompsci@protonmail.com>
2025-07-01 19:50:41 +0200

    fix: spellcheck part of commit message after (feat|fix|...):

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 # RUN THIS INSIDE .git/hooks OF YOUR GIT REPO
 
-ln -s $HOME/developer/git-hooks/commit-msg .
-ln -s $HOME/developer/git-hooks/pre-commit .
+cat extra_words | xargs -I {} echo -e "*{}\\n#" | hunspell -d en_US -a
+
+ln -s $HOME/developer/git-hooks/commit-msg .git/hooks
+ln -s $HOME/developer/git-hooks/pre-commit .git/hooks
